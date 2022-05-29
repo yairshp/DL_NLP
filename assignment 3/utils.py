612b088727019df24b4cbe44ef173c3f7dd5bb88
yairshp <yairsh39@gmail.com>
2022-05-29 22:14:28 +0300

    ass3 - option c - writing lstm

--- assignment 3/utils.py
+++ assignment 3/utils.py
@@ -1,10 +1,13 @@
+import numpy as np
 import pandas as pd
+import torch
 
-POS_DEBUG_PATH = '/home/yair/Documents/University/Deep Learning for NLP/assignment 3/data/pos/debug'
+POS_DEBUG_PATH = '/home/yair/Documents/University/Deep Learning for NLP/assignment 3/data/pos/big_debug_50000'
 POS_TRAIN_PATH = '/home/yair/Documents/University/Deep Learning for NLP/assignment 3/data/pos/train'
 POS_DEV_PATH = '/home/yair/Documents/University/Deep Learning for NLP/assignment 3/data/pos/dev'
 POS_TEST_PATH = '/home/yair/Documents/University/Deep Learning for NLP/assignment 3/data/pos/test'
-UNKNOWN = 'UUUNNNKKK'
+EMBEDDING_PATH = '/home/yair/Documents/University/Deep Learning for NLP/assignment 3/embeddings'
+UNKNOWN = 'UUUNKKK'
 POS_TAGS = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS',
             'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO', 'UH', 'VB', 'VBD', 'VBG', 'VBN', 'VBP',
             'VBZ', 'WDT', 'WP', 'WP$', 'WRB', '``', ',', '.', '\'\'', '(', ')', ':', '$', '#']
@@ -12,6 +15,8 @@ CHARS = ['(', ';', 'z', 'k', 'T', '5', '}', '?', '2', 'r', 'a', 'y', 'D', 's', '
          'o', 'L', 'x', 'q', '`', 'm', 'g', '#', '9', '=', 'N', 'e', '1', 'P', 'I', 'd', '{', ',', '*', 'A', 'n', "'",
          'Q', 'w', 'F', 'Z', '!', 'S', 'C', 'H', 'W', '%', 'f', '/', 'c', 'i', 'E', 'U', 'M', '@', 'l', '-', '6', ':',
          'J', '7', 'R', 'u', 'Y', 'B', '.', 'p', 'j', 'V', 'O', 't', 'b', 'h', '0', 'X', '3', '$', '']
+PREFIX_LEN = 3
+SUFFIX_LEN = 3
 
 
 def is_end_of_sentence(word):
@@ -30,3 +35,46 @@ def create_corpus(corpus_path):
         words_counter += 1
     corpus[UNKNOWN] = words_counter
     return corpus
+
+
+def get_suffix(word):
+    if len(word) <= SUFFIX_LEN:
+        return word
+    return word[-SUFFIX_LEN:]
+
+
+def get_prefix(word):
+    if len(word) <= PREFIX_LEN:
+        return word
+    return word[:PREFIX_LEN]
+
+
+def create_corpus_with_subwords(vocab_path):
+    with open(vocab_path, 'r') as r:
+        vocab = r.readlines()
+    words = [w[:-1] for w in vocab]
+    words_counter = 0
+    prefix_counter = 0
+    suffix_counter = 0
+    corpus = {}
+    prefixes = {}
+    suffixes = {}
+    for word in words:
+        if word not in corpus:
+            corpus[word] = words_counter
+            words_counter += 1
+        prefix = get_prefix(word)
+        if prefix not in prefixes:
+            prefixes[prefix] = prefix_counter
+            prefix_counter += 1
+        suffix = get_suffix(word)
+        if suffix not in suffixes:
+            suffixes[suffix] = suffix_counter
+            suffix_counter += 1
+    corpus[UNKNOWN] = words_counter
+    return corpus, prefixes, suffixes
+
+
+def create_embedding_matrix(vectors_path):
+    vectors = np.loadtxt(vectors_path)
+    return torch.stack([torch.tensor(v) for v in vectors])
