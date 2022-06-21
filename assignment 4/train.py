7edfea4ef5422eb8dda12dd42997ef740652eadc
yairshp <yairsh39@gmail.com>
2022-06-21 19:00:52 +0300

    ass4 - 80% accuracy

--- assignment 4/train.py
+++ assignment 4/train.py
@@ -18,7 +18,7 @@ def train(train_data_iter, dev_data_iter, embedding_matrix):
     for epoch in range(utils.EPOCHS):
         model.train()
         epoch_loss = 0
-
+        counter = 0
         for batch_num, sequence_batch in enumerate(train_data_iter):
             optimizer.zero_grad()
 
@@ -27,15 +27,36 @@ def train(train_data_iter, dev_data_iter, embedding_matrix):
 
             outputs = model(premise_sentences.to(utils.device), hypothesis_sentences.to(utils.device),
                             premise_lengths, hypothesis_lengths)
-            loss = criterion(outputs, sequence_batch.label - 1)
+            loss = criterion(outputs.to(utils.device), sequence_batch.label.to(utils.device) - 1)
 
             loss.backward()
             nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
             optimizer.step()
 
-            loss += epoch_loss
+            epoch_loss += loss
+            counter += 1
 
-        print(f'epoch {epoch + 1}/{utils.EPOCHS}: loss - {epoch_loss}')
+        accuracy = validate(model, dev_data_iter)
+        print(f'epoch {epoch + 1}/{utils.EPOCHS}: loss - {epoch_loss / counter}, accuracy - {accuracy}')
+
+
+def validate(model, dev_data_iter):
+    model.eval()
+
+    correct = 0
+    samples = 0
+
+    with torch.no_grad():
+        for batch_num, sequence_batch in enumerate(dev_data_iter):
+            premise_sentences, premise_lengths = sequence_batch.premise
+            hypothesis_sentences, hypothesis_length = sequence_batch.hypothesis
+            outputs = model(premise_sentences.to(utils.device), hypothesis_sentences.to(utils.device),
+                            premise_lengths, hypothesis_length)
+            _, predictions = outputs.max(1)
+            correct += float((predictions == sequence_batch.label - 1).sum())
+            samples += float(predictions.size(0))
+
+    return correct / samples
 
 
 def main():
--- assignment 4/utils.py
+++ assignment 4/utils.py
@@ -1,3 +1,5 @@
+import re
+import numpy as np
 import torch
 import torchtext.legacy as torchtext
 
@@ -13,13 +15,22 @@ EPOCHS = 3
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
+def generate_random_embedding(w):
+    x = np.random.uniform(-0.05, 0.05, EMBEDDING_DIM)
+    return torch.tensor(x)
+
+
 def get_data_and_embeddings():
     x = torchtext.data.Field(init_token='</begin>', lower=True, batch_first=True, include_lengths=True)
     y = torchtext.data.Field(sequential=False)
 
     train_data, dev_data, test_data = torchtext.datasets.SNLI.splits(text_field=x, label_field=y)
 
-    x.build_vocab(train_data, min_freq=1, vectors='glove.6B.300d')
+    train_data = differentiate_input(train_data)
+    dev_data = differentiate_input(dev_data)
+    test_data = differentiate_input(test_data)
+
+    x.build_vocab(train_data, min_freq=1, vectors='glove.6B.300d', unk_init=generate_random_embedding)
     y.build_vocab(train_data)
 
     embedding_matrix = x.vocab.vectors
@@ -30,4 +41,14 @@ def get_data_and_embeddings():
 
     return train_iter, dev_iter, test_iter, embedding_matrix
 
+
+def differentiate_input(data):
+    for example in data:
+        original_hypothesis = example.hypothesis
+        original_hypothesis = [re.sub('[^A-Za-z0-9]', '', w) for w in original_hypothesis]
+        original_premise = example.premise
+        original_premise = [re.sub('[^A-Za-z0-9]', '', w) for w in original_premise]
+        example.hypothesis = [w for w in original_hypothesis if w not in original_premise]
+        example.premise = [w for w in original_premise if w not in original_hypothesis]
+    return data
 
